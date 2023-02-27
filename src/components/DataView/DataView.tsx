b25bdc12a1d1ff289a13c37bd262e44e119a5095
Ryan O'Malley <rd.omalley@comcast.net>
2023-02-26 21:45:13 -0600

    Wrapped BarGraph in InfoCards

--- src/components/DataView/DataView.tsx
+++ src/components/DataView/DataView.tsx
@@ -1,104 +1,121 @@
 import React from 'react';
 import { ResponsiveBar } from '@nivo/bar'
 import mockData from "../../mock/data.json";
-import { Box } from '@material-ui/core';
+import { Box, Container, Grid } from '@material-ui/core';
+import { InfoCard } from '@backstage/core-components';
 
 export const DataView = ({ }: {}) => {
     return (
-        <Box sx={
-            {
-                width: 600,
-                height: 300
-            }
-        }>
-            <ResponsiveBar
-                data={mockData}
-                indexBy="severity"
-                keys={["count"]}
-                margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-                labelTextColor={"white"}
-                padding={0.2}
-                valueScale={{ type: 'linear' }}
-                indexScale={{ type: 'band', round: true }}
-                colors={{ scheme: "category10" }}
-                colorBy="indexValue"
-                defs={[
-                    {
-                        id: 'dots',
-                        type: 'patternDots',
-                        background: 'inherit',
-                        color: '#38bcb2',
-                        size: 4,
-                        padding: 1,
-                        stagger: true
-                    },
-                    {
-                        id: 'lines',
-                        type: 'patternLines',
-                        background: 'inherit',
-                        color: '#eed312',
-                        rotation: -45,
-                        lineWidth: 6,
-                        spacing: 10
-                    }
-                ]}
-                borderColor={{
-                    from: 'color',
-                    modifiers: [
-                        [
-                            'darker',
-                            1.6
-                        ]
-                    ]
-                }}
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: 'Severity',
-                    legendPosition: 'middle',
-                    legendOffset: 32
-                }}
-                axisLeft={{
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: 'Count',
-                    legendPosition: 'middle',
-                    legendOffset: -40
-                }}
-                labelSkipWidth={12}
-                labelSkipHeight={12}
-                legends={[
-                    {
-                        dataFrom: 'indexes',
-                        anchor: 'bottom-right',
-                        direction: 'column',
-                        justify: false,
-                        translateX: 120,
-                        translateY: 0,
-                        itemsSpacing: 2,
-                        itemWidth: 100,
-                        itemHeight: 20,
-                        itemDirection: 'left-to-right',
-                        itemOpacity: 0.85,
-                        symbolSize: 20,
-                        effects: [
-                            {
-                                on: 'hover',
-                                style: {
-                                    itemOpacity: 1
+        <Grid container spacing={2}>
+            <Grid item>
+                <InfoCard variant='flex'>
+                    <Box style={{
+                        width: "30rem",
+                        height: "15rem"
+                    }}
+                    >
+                        <ResponsiveBar
+                            data={mockData}
+                            indexBy="severity"
+                            keys={["count"]}
+                            margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+                            labelTextColor={"white"}
+                            padding={0.2}
+                            valueScale={{ type: 'linear' }}
+                            indexScale={{ type: 'band', round: true }}
+                            colors={{ scheme: "category10" }}
+                            colorBy="indexValue"
+                            defs={[
+                                {
+                                    id: 'dots',
+                                    type: 'patternDots',
+                                    background: 'inherit',
+                                    color: '#38bcb2',
+                                    size: 4,
+                                    padding: 1,
+                                    stagger: true
+                                },
+                                {
+                                    id: 'lines',
+                                    type: 'patternLines',
+                                    background: 'inherit',
+                                    color: '#eed312',
+                                    rotation: -45,
+                                    lineWidth: 6,
+                                    spacing: 10
                                 }
-                            }
-                        ]
-                    }
-                ]}
-                role="application"
-                ariaLabel="Nivo bar chart demo"
-                barAriaLabel={function (e) { return e.indexValue.toString() }}
-            />
-        </Box>
+                            ]}
+                            borderColor={{
+                                from: 'color',
+                                modifiers: [
+                                    [
+                                        'darker',
+                                        1.6
+                                    ]
+                                ]
+                            }}
+                            axisTop={null}
+                            axisRight={null}
+                            axisBottom={{
+                                tickSize: 5,
+                                tickPadding: 5,
+                                tickRotation: 0,
+                                legend: 'Severity',
+                                legendPosition: 'middle',
+                                legendOffset: 32
+                            }}
+                            axisLeft={{
+                                tickSize: 5,
+                                tickPadding: 5,
+                                tickRotation: 0,
+                                legend: 'Count',
+                                legendPosition: 'middle',
+                                legendOffset: -40
+                            }}
+                            labelSkipWidth={12}
+                            labelSkipHeight={12}
+                            legends={[
+                                {
+                                    dataFrom: 'indexes',
+                                    anchor: 'bottom-right',
+                                    direction: 'column',
+                                    justify: false,
+                                    translateX: 120,
+                                    translateY: 0,
+                                    itemsSpacing: 2,
+                                    itemWidth: 100,
+                                    itemHeight: 20,
+                                    itemDirection: 'left-to-right',
+                                    itemOpacity: 0.85,
+                                    symbolSize: 20,
+                                    effects: [
+                                        {
+                                            on: 'hover',
+                                            style: {
+                                                itemOpacity: 1
+                                            }
+                                        }
+                                    ]
+                                }
+                            ]}
+                            role="application"
+                            ariaLabel="Nivo bar chart demo"
+                            barAriaLabel={function (e) { return e.indexValue.toString() }}
+                        />
+                    </Box>
+                </InfoCard>
+            </Grid>
+            <Grid item>
+                <InfoCard>
+
+                </InfoCard>
+            </Grid>
+            <Grid item>
+                <InfoCard>
+
+                </InfoCard>
+            </Grid>
+
+        </Grid>
     );
 };
