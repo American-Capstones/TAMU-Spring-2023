7835bb634d6f2479b5007e8c30d9e3d9d14217f5
Sean Davis <sdavisde@gmail.com>
2023-03-05 14:53:22 -0600

    adding line graph and mock data for it

--- src/components/DataView/DataView.tsx
+++ src/components/DataView/DataView.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { ResponsiveBar } from '@nivo/bar'
+import { ResponsiveBar } from '@nivo/bar';
+import { ResponsiveLine } from '@nivo/line';
 import mockData from "../../mock/data.json";
+import lineMockData from '../../mock/lineMock.json';
 import { Box, Container, Grid } from '@material-ui/core';
 import { InfoCard } from '@backstage/core-components';
 
 export const DataView = ({ }: {}) => {
     return (
-        <Grid container spacing={2}>
+        <Grid container spacing={2} justifyContent='center'>
             <Grid item>
                 <InfoCard variant='flex'>
                     <Box style={{
@@ -15,6 +17,7 @@ export const DataView = ({ }: {}) => {
                     }}
                     >
                         <ResponsiveBar
+                            colors={{scheme: 'red_blue'}}
                             data={mockData}
                             indexBy="severity"
                             keys={["count"]}
@@ -23,7 +26,6 @@ export const DataView = ({ }: {}) => {
                             padding={0.2}
                             valueScale={{ type: 'linear' }}
                             indexScale={{ type: 'band', round: true }}
-                            colors={{ scheme: "category10" }}
                             colorBy="indexValue"
                             defs={[
                                 {
@@ -102,19 +104,88 @@ export const DataView = ({ }: {}) => {
                             ariaLabel="Nivo bar chart demo"
                             barAriaLabel={function (e) { return e.indexValue.toString() }}
                         />
+                        
                     </Box>
                 </InfoCard>
             </Grid>
             <Grid item>
+                <InfoCard variant='flex'>
+                    <Box style={{
+                        width:"30rem",
+                        height:"15rem"
+                    }}>
+                        <ResponsiveLine
+                            colors={{scheme: 'red_blue'}}
+                            data={lineMockData}
+                            margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                            xScale={{ type: 'point' }}
+                            yScale={{
+                                type: 'linear',
+                                min: 'auto',
+                                max: 'auto',
+                                stacked: true,
+                                reverse: false
+                            }}
+                            yFormat=" >-.2f"
+                            axisTop={null}
+                            axisRight={null}
+                            axisBottom={{
+                                tickSize: 5,
+                                tickPadding: 5,
+                                tickRotation: 0,
+                                legend: 'transportation',
+                                legendOffset: 36,
+                                legendPosition: 'middle'
+                            }}
+                            axisLeft={{
+                                tickSize: 5,
+                                tickPadding: 5,
+                                tickRotation: 0,
+                                legend: 'count',
+                                legendOffset: -40,
+                                legendPosition: 'middle'
+                            }}
+                            pointSize={10}
+                            pointColor={{ theme: 'background' }}
+                            pointBorderWidth={2}
+                            pointBorderColor={{ from: 'serieColor' }}
+                            pointLabelYOffset={-12}
+                            useMesh={true}
+                            legends={[
+                                {
+                                    anchor: 'bottom-right',
+                                    direction: 'column',
+                                    justify: false,
+                                    translateX: 100,
+                                    translateY: 0,
+                                    itemsSpacing: 0,
+                                    itemDirection: 'left-to-right',
+                                    itemWidth: 80,
+                                    itemHeight: 20,
+                                    itemOpacity: 0.75,
+                                    symbolSize: 12,
+                                    symbolShape: 'circle',
+                                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                                    effects: [
+                                        {
+                                            on: 'hover',
+                                            style: {
+                                                itemBackground: 'rgba(0, 0, 0, .03)',
+                                                itemOpacity: 1
+                                            }
+                                        }
+                                    ]
+                                }
+                            ]}
+                        />
+                    </Box>
+                </InfoCard>
+            </Grid>
+            {/* <Grid item>
                 <InfoCard>
 
                 </InfoCard>
-            </Grid>
-            <Grid item>
-                <InfoCard>
-
-                </InfoCard>
-            </Grid>
+            </Grid> */}
 
         </Grid>
     );
