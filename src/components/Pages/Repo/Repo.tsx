8c988a7bbe97434230cc9493c645e2912712d2eb
Sean Davis <sdavisde@gmail.com>
2023-03-13 14:13:08 -0500

    refactored Repo.tsx to make functionality more clear

--- src/components/Pages/Repo/Repo.tsx
+++ src/components/Pages/Repo/Repo.tsx
@@ -12,44 +12,33 @@ import { sortVulnData } from '../../../utils/functions';
 export const Repo = ({  }: { }) => {
     const { repoName } = useParams();
     const defaultValues = {critical: [], high: [], moderate: [], low: []}
-    // This state variable isn't very representative of the information it's storing for the UI.
-    // Really this is being used when filling out the VulnList's. 
-    // So maybe call it "shownRepoVulns" or "activeVulns" or something like that
-    const [ repoInfo, setRepoInfo ] = useState<RepoVulns>(defaultValues);
-    // I like this just fine, think it makes sense given it's context.
+    const [ shownRepoVulns, setShownRepoVulns ] = useState<RepoVulns>(defaultValues);
     const [ allRepoInfo, setAllRepoInfo ] = useState<RepoVulns>(defaultValues);
 
-    if (repoName && repoInfo == defaultValues) {
-        // This call should only have to be made once, and you can call each set function in the then() fn
+    if (repoName && shownRepoVulns == defaultValues) {
         getVulnerabilitiesFromRepo(repoName, 'Baggage-Claim-Incorporated')
         .then((data) => {
-            setRepoInfo(sortVulnData(data))
-        })
-        getVulnerabilitiesFromRepo(repoName, 'Baggage-Claim-Incorporated')
-        .then((data) => {
-            setAllRepoInfo(sortVulnData(data))
+            const sortedData = sortVulnData(data);
+            setShownRepoVulns(sortedData);
+            setAllRepoInfo(sortedData);
         })
     }
 
-    // Probably best for this to have a more descriptive name, maybe "openVulns"?
-    const newObject = {
-      critical: repoInfo.critical.filter(vuln => vuln.state.toLowerCase() == "open"),
-      high: repoInfo.high.filter(vuln => vuln.state.toLowerCase() == "open"),
-      moderate: repoInfo.moderate.filter(vuln => vuln.state.toLowerCase() == "open"),
-      low: repoInfo.low.filter(vuln => vuln.state.toLowerCase() == "open"),
+    const openVulns = {
+      critical: allRepoInfo.critical.filter(vuln => vuln.state.toLowerCase() == "open"),
+      high: allRepoInfo.high.filter(vuln => vuln.state.toLowerCase() == "open"),
+      moderate: allRepoInfo.moderate.filter(vuln => vuln.state.toLowerCase() == "open"),
+      low: allRepoInfo.low.filter(vuln => vuln.state.toLowerCase() == "open"),
     }
     
     const openFilter = (event: React.FormEvent<HTMLInputElement>) => {
-      const target = event.target as HTMLInputElement;
-      // I think both of these conditionals might be trying to say
-      // "if (switch_state) then set active vulns to ____"
-      // so you probably want to sub each of the ___ values 
-      // instead of "repoInfo" in these if statements.
-      if (target.checked && repoInfo) {
-        setRepoInfo(newObject);
-      } else if (!target.checked && repoInfo){
-        setRepoInfo(allRepoInfo);
-      }
+        const target = event.target as HTMLInputElement;
+
+        if (target.checked) {
+            setShownRepoVulns(openVulns);
+        } else if (!target.checked){
+            setShownRepoVulns(allRepoInfo);
+        }
     }
     
     return (
@@ -59,22 +48,22 @@ export const Repo = ({  }: { }) => {
             <Grid container spacing={1}>
                 <Grid item xs={3}>
                     <InfoCard title="Critical">
-                        <VulnList vulns={repoInfo.critical} />
+                        <VulnList vulns={shownRepoVulns.critical} />
                     </InfoCard>
                 </Grid>
                 <Grid item xs={3}>
                     <InfoCard title="High">
-                        <VulnList vulns={repoInfo.high} />
+                        <VulnList vulns={shownRepoVulns.high} />
                     </InfoCard>
                 </Grid>
                 <Grid item xs={3}>
                     <InfoCard title="Moderate">
-                        <VulnList vulns={repoInfo.moderate} />
+                        <VulnList vulns={shownRepoVulns.moderate} />
                     </InfoCard>
                 </Grid>
                 <Grid item xs={3}>
                     <InfoCard title="Low">
-                        <VulnList vulns={repoInfo.low} />
+                        <VulnList vulns={shownRepoVulns.low} />
                     </InfoCard>
                 </Grid>
             </Grid>
