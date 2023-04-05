ac2158aad9448b0a4e6be04c8db628383f9a9d4c
Ryan O'Malley <rd.omalley@comcast.net>
2023-04-05 14:24:00 -0500

    ui(team page): chip for reoccuring vulns

--- src/components/Pages/Team/Team.tsx
+++ src/components/Pages/Team/Team.tsx
@@ -4,8 +4,8 @@ import { useNavigate, useParams, useLocation } from 'react-router-dom';
 import ReactLoading from "react-loading";
 import { DataContext } from '../../Root/Root';
 import { Team } from '../../../utils/types';
-import { Table } from '@backstage/core-components';
-import { Grid } from '@material-ui/core';
+import { GitHubIcon, GroupIcon, Table } from '@backstage/core-components';
+import { Chip, Grid, IconButton, Typography } from '@material-ui/core';
 import { makeBarData, makeLineData } from '../../../utils/functions';
 import { Alert, Skeleton } from '@mui/material';
 import { useGetTeamVulns } from '../../../hooks/useGetTeamVulns';
@@ -16,11 +16,45 @@ const emptyContent = () => {
     )
 }
 
-export const TeamPage = ({} : {}) => {
+export const TeamPage = ({ }: {}) => {
     const { orgName, teamName } = useParams();
     const { data: teamData, loading, error } = useGetTeamVulns(orgName, teamName);
     const navigate = useNavigate();
     const location = useLocation();
+
+    const getColorStyling = (numVulns: number) => {
+        let style : React.CSSProperties = {}
+        if ( numVulns == 0 ){
+            style = {
+                // green background
+                backgroundColor: 'green',
+                color: 'white'
+            }
+        }
+        else if (numVulns <= 2) {
+            style = {
+                // yellow background
+                backgroundColor: '#ffeb3b',
+                color: '#523b02'
+            }
+
+        }
+        else if (numVulns < 6) {
+            style = {
+                // orange background
+                backgroundColor: '#ff9800',
+                color: 'white'
+            }
+        }
+        else {
+            style = {
+                // red background
+                backgroundColor: 'red',
+                color: 'white'
+            }
+        }
+        return style
+    };
 
     const goToRepo = (event: React.MouseEvent | undefined, rowData: any) => {
         navigate(`./${rowData.name}`, { replace: true });
@@ -31,22 +65,56 @@ export const TeamPage = ({} : {}) => {
     }
 
     const cols = [
-        {title: 'Repository Name', field: 'name'},
-        {title: 'critical', field: 'critical'},
-        {title: 'high', field: 'high'},
-        {title: 'moderate', field: 'moderate'},
-        {title: 'low', field: 'low'},
-        {title: 'topics', field: 'repositoryTopics'}
+        { title: 'Repository Name', field: 'name' },
+        { title: 'critical', field: 'critical' },
+        { title: 'high', field: 'high' },
+        { title: 'moderate', field: 'moderate' },
+        { title: 'low', field: 'low' },
+        { title: 'topics', field: 'repositoryTopics' }
     ]
     const filters: any[] = [];
     const title = `${teamName}'s Repositories`;
-    
+
     return (
         <>
-            {location.state && 
-                <Alert severity='error' style={{marginBottom: '1rem'}}>{location.state}</Alert>
+            {location.state &&
+                <Alert severity='error' style={{ marginBottom: '1rem' }}>{location.state}</Alert>
             }
-            <h1>{(teamData && !loading) ? teamName : ""}</h1>
+            <div style={{
+                marginBottom: '1.64rem'
+            }}>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: '.48rem'
+                }}>
+                    <Chip label='Team' icon={<GroupIcon />} style={{ paddingLeft: '.48rem' }} />
+
+                    {teamData?.offenses != undefined &&
+                        <Chip style={getColorStyling(teamData.offenses)} label={`${teamData?.offenses} Reoccurences`} />
+                    }
+                </div>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: '.48rem'
+                }}>
+                    {loading && <Skeleton variant='text'></Skeleton>}
+                    {!loading &&
+                        <Typography style={{
+                            marginTop: 0,
+                            marginBottom: 0
+                        }} variant='h3'>{(teamData && !loading) ? teamName : ""}</Typography>}
+                    {!loading &&
+                        <IconButton target='_blank' href='https://github.com' aria-label='Go to Team Page on GitHub'>
+                            <GitHubIcon />
+                        </IconButton>
+                    }
+                </div>
+            </div>
+            {teamData?.offenses != undefined && teamData.offenses > 0 && <Chip label={`Offenses: ${teamData.offenses}`} />}
             <Grid container spacing={6} direction='column'>
                 <Grid item>
                     <Graphs barData={makeBarData(teamData)} lineData={makeLineData(teamData)} isLoading={loading} />
@@ -55,28 +123,28 @@ export const TeamPage = ({} : {}) => {
                 <Grid item></Grid>
                 <Grid item>
                     {(loading || !teamData) ?
-                    <div style={ {display: 'flex', justifyContent: 'center'}}>
-                        <Skeleton variant="rectangular">
-                            <Table title={title}
-                                options={{ search: true, paging: true }}
-                                columns={cols}
-                                data={[]}
-                                onRowClick={goToRepo}
-                                filters={filters}
-                                emptyContent={emptyContent} />
-                        </Skeleton>
-                    </div>
-                    :
-                    <Table
-                        title="Repositories"
-                        subtitle={teamName}
-                        options={{ search: true, paging: true }}
-                        columns={cols}
-                        data={teamData!.repos}
-                        onRowClick={goToRepo}
-                        filters={filters}
-                        emptyContent={emptyContent}
-                    />}
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <Skeleton variant="rectangular">
+                                <Table title={title}
+                                    options={{ search: true, paging: true }}
+                                    columns={cols}
+                                    data={[]}
+                                    onRowClick={goToRepo}
+                                    filters={filters}
+                                    emptyContent={emptyContent} />
+                            </Skeleton>
+                        </div>
+                        :
+                        <Table
+                            title="Repositories"
+                            subtitle={teamName}
+                            options={{ search: true, paging: true }}
+                            columns={cols}
+                            data={teamData!.repos}
+                            onRowClick={goToRepo}
+                            filters={filters}
+                            emptyContent={emptyContent}
+                        />}
                 </Grid>
             </Grid>
 
