7a9dc71034c7ff7d871e754359bf36750228aa49
Sean Davis <sdavisde@gmail.com>
2023-04-10 12:17:46 -0500

    updating logic for chips

--- src/components/Pages/Topic/Topic.tsx
+++ src/components/Pages/Topic/Topic.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Graphs } from '../../Graphs';
 import { useNavigate, useParams, useLocation } from 'react-router-dom';
 import { Table } from '@backstage/core-components';
-import { Grid } from '@material-ui/core';
+import { Chip, Grid, Typography } from '@material-ui/core';
 import { makeBarData, makeLineData } from '../../../utils/functions';
+import TagIcon from '@mui/icons-material/Tag';
 import { useGetTopicVulns } from '../../../hooks/useGetTopicVulns';
 import { Alert, Skeleton } from '@mui/material';
 
-const emptyContent = <h1>This team has no associated repositories</h1>;
+const emptyContent = <h1>This topic has no associated repositories</h1>;
 
 export const TopicPage = ({ }: {}) => {
     const { orgName, topicName } = useParams();
@@ -38,6 +39,35 @@ export const TopicPage = ({ }: {}) => {
             {location.state &&
                 <Alert severity='error' style={{ marginBottom: '1rem' }}>{location.state}</Alert>
             }
+
+            <div style={{
+                marginBottom: '1.64rem'
+            }}>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: '.48rem'
+                }}>
+                    {(topicData && !loading) &&
+                        <Chip
+                            label='Topic'
+                            icon={<TagIcon sx={{ color: '#333333' }} />}
+                            style={{ paddingLeft: '.48rem' }} />
+                    }
+                </div>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: '.48rem'
+                }}>
+                    <Typography style={{
+                        marginTop: 0,
+                        marginBottom: 0
+                    }} variant='h3'>{(topicData && !loading) ? topicName : ""}</Typography>
+                </div>
+            </div>
             <Grid container spacing={6} direction='column'>
                 <Grid item>
                     <Graphs barData={makeBarData(topicData)} lineData={makeLineData(topicData)} />
